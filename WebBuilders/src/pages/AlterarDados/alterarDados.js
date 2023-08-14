b38988e5cd0d31e2c65749805da935ba978eac79
Matheus Scarpim <108617060+MatheusScarpim@users.noreply.github.com>
2023-08-14 20:41:58 -0300

    AlterarDadosUsuario

--- WebBuilders/src/pages/AlterarDados/alterarDados.js
+++ WebBuilders/src/pages/AlterarDados/alterarDados.js
@@ -9,6 +9,29 @@ router.use(bodyParser.urlencoded({
 }));
 
 router.get('/alterarUsuario', (req, res) => {
-    res.send(req.session.login)
+    con.query('SELECT * FROM customer where email =?', req.session.login, (err, users) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        console.log(users)
+        let user = users[0];
+        res.render(path.join(__dirname + "/AlteraDadosUsuario", 'index.ejs'), {
+            user
+        });
+    });
 });
+router.post('/alterarUsuario', (req, res) => {
+    const reqBody = req.body;
+    con.query('Update customer set ? where email =?', [reqBody, req.session.login], (err, users) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.redirect("/")
+    });
+});
+
 module.exports = router;
