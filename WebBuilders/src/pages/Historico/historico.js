8706ad9a0290390f8607a0ec9ba38544cf2b98f9
LuanPaD <111983442+LuanPaD@users.noreply.github.com>
2023-09-04 21:51:02 -0300

    historico
    .

--- WebBuilders/src/pages/Historico/historico.js
+++ WebBuilders/src/pages/Historico/historico.js
@@ -16,6 +16,7 @@ router.get('/historico', (req, res) => {
         res.status(500).send('Error fetching books');
         return;
       }
+
       con.query('select h.date,h.status_book from actions ac INNER join historic h on (ac.id_action = h.id_action) inner join book b on (ac.id_book = b.id_book) where ac.id_customer = ? order by h.date',req.session.id_customer, (err, historic) => {
         if (err) {
           console.error('Error fetching books:', err);
@@ -23,7 +24,7 @@ router.get('/historico', (req, res) => {
           return;
         }
         res.render(path.join(__dirname, 'index.ejs'), {
-            books,historic,names: req.session.names
+            books, historic ,names: req.session.names
           });
         });
     });
