99dbb13cdd4f8b1bf634254732e55ab0e017922f
LuanPaD <111983442+LuanPaD@users.noreply.github.com>
2023-09-11 19:12:12 -0300

    .
    .

--- WebBuilders/src/pages/AlterarDados/alterarDados.js
+++ WebBuilders/src/pages/AlterarDados/alterarDados.js
@@ -1,3 +1,4 @@
+//LIBS
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -7,10 +8,28 @@ const cargo = require('../Cargo/cargo');
 const verificarAdm = require('../Cargo/cargo');
 const checkRole = require('../Cargo/cargo');
 const checkCargo = require('../Cargo/cargo');
+const multer = require('multer');
+const fs = require('fs');
+const { Console } = require('console');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/uploads'); // Specify the path where files will be stored
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+    storage: storage
+  });
 
 router.use(bodyParser.urlencoded({
     extended: true
 }));
+
 //Usuário
 router.get('/alterarUsuario', (req, res) => {
     con.query('SELECT * FROM customers where email =?', req.session.login, (err, users) => {
@@ -25,6 +44,7 @@ router.get('/alterarUsuario', (req, res) => {
         {  user,  names: req.session.names});
     });
 });
+
 router.post('/alterarUsuario', (req, res) => {
     const reqBody = req.body;
     console.log(reqBody)
@@ -38,28 +58,41 @@ router.post('/alterarUsuario', (req, res) => {
     });
 });
 
-//Livros
-router.get('/alterarLivro', checkCargo("A"), (req, res) => {
-        let id = req.query.id;
-        con.query('SELECT * FROM book where id_book =?', id, (err, books) => {
-            if (err) {
-                console.error('Error fetching books:', err);
-                res.status(500).send('Error fetching books');
-                return;
-            }
-            let book = books[0]
-            res.render(path.join(__dirname + "/AlteraDadosLivros", 'index.ejs'), {
-                book
-            });
-        });
-});
-router.post('/alterarLivro', checkCargo("A"), (req, res) => {
-    const id = req.query.id;
-    let reqBody = req.body;
-    con.query('Update book set ? where id_book = ?', [reqBody, id], (err, books) => {
+//Receber dados Livros
+router.get('/alterarLivro', checkCargo("A"),  (req, res) => {
+    let id = req.query.id;
+    const reqBody = req.body;
+
+    con.query('SELECT * FROM book where id_book =?', id, (err, books) => {
         if (err) {
             console.error('Error fetching books:', err);
-            res.status(500).send('Error fetching books');   
+            res.status(500).send('Error fetching books');
+            return;
+        }
+        let book = books[0]
+        res.render(path.join(__dirname + "/AlteraDadosLivros", 'index.ejs'), {
+            book
+        });
+    });
+});
+
+//Atualizar
+router.post('/alterarLivro', checkCargo("A"), upload.single('image'),(req, res) => {
+    const id = req.query.id;
+    let reqBody = req.body;
+
+    //Temporario
+    //if (!(req.file === undefined)  !(req.file.path === undefined)) {
+        console.log("\n\n\\n\n\n\n\\n\n\n\n\n"+req.file.path);
+        const imagePath = req.file.path;
+        const imageBuffer = fs.readFileSync(imagePath);
+        reqBody.foto = imageBuffer;
+    //}
+    
+    con.query('Update book set ? where id_book = ?', [reqBody, id], (err, books) => {
+        if (err) {
+            console.error('Error inserting books:', err);
+            res.status(500).send('Error inserting book');   
             return;
         } 
         res.redirect("/livros")
